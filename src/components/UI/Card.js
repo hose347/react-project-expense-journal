d040b510e60c1af60a441608360b00154dd4e693
hose347 <hosse.css@gmail.com>
2022-05-23 19:32:11 +0200

    click hanlder

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,9 +2,11 @@ import "./Card.css";
 import _, { join } from "lodash";
 
 function Card(props) {
+  console.log(props.children);
   // const classes = "card" + props.className;
   const classes = join(["card", props.className], " ");
-  console.log("classess", classes);
+  // console.log("classess", classes);
   return <div className={classes}>{props.children}</div>;
 }
+
 export default Card;
